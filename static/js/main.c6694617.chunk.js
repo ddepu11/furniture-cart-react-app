614cc64d1b204ad0ab867fab3d512f2537d56b08
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-15 19:43:28 +0530

    Updates

--- static/js/main.c6694617.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);n(1);var c=n(3),a=n.n(c),r=(n(8),n(0));var s=function(){return Object(r.jsx)("div",{className:"container"})};a.a.render(Object(r.jsx)(s,{}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.c6694617.chunk.js.map
